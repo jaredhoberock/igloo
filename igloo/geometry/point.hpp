ba2a51d268dc9d065bb871b2906722589faac5c1
Jared Hoberock <jaredhoberock@gmail.com>
2014-01-14 00:55:30 -0800

    Implement point difference.

--- igloo/geometry/point.hpp
+++ igloo/geometry/point.hpp
@@ -13,6 +13,11 @@ class point : float3
   private:
     typedef float3 super_t;
 
+    /*! Creates a new point from a float3.
+     *  \param v The float3 to copy.
+     */
+    inline point(const float3 &v) : super_t(v) {}
+
   public:
     /*! Creates a new default constructed point.
      */
@@ -24,6 +29,15 @@ class point : float3
      *  \param z The z coordinate of the point.
      */
     inline point(float x, float y, float z) : super_t(x,y,z) {}
+
+    /*! point difference.
+     *  \param rhs The point to subtract.
+     *  \return The vector pointing from rhs to *this.
+     */
+    inline float3 operator-(const point &rhs) const
+    {
+      return point(*this - rhs);
+    } // end operator-()
 
     using super_t::operator[];
     using super_t::operator float *;
