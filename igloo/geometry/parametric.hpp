860d29513448145c108f8fa6775ef1bd1bd217ec
Jared Hoberock <jaredhoberock@gmail.com>
2014-01-14 00:55:02 -0800

    Implement parametric coordinate type.

---
+++ igloo/geometry/parametric.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <igloo/geometry/vector.hpp>
+
+namespace igloo
+{
+
+
+/*! A parametric represents parametric coordinates in the plane.
+ */
+class parametric : float2
+{
+  private:
+    typedef float2 super_t;
+
+  public:
+    /*! Creates a new default constructed parametric.
+     */
+    inline parametric() : super_t() {}
+
+    /*! Creates a new point.
+     *  \param x The x coordinate of the parametric coordinates.
+     *  \param y The y coordinate of the parametric coordinates.
+     */
+    inline parametric(float x, float y) : super_t(x,y) {}
+
+    using super_t::operator[];
+    using super_t::operator float *;
+
+    using super_t::x;
+    using super_t::y;
+}; // end parametric
+
+
+} // end igloo
+
--- igloo/geometry/vector.hpp
+++ igloo/geometry/vector.hpp
@@ -10,6 +10,38 @@ namespace igloo
 
 template<typename T, std::size_t> class vector;
 
+
+template<typename T>
+class vector<T,2> : public detail::vector_base<vector<T,2>, T, 2>
+{
+  private:
+    typedef detail::vector_base<vector<T,2>, T, 2> super_t;
+
+  public:
+    typedef typename super_t::value_type value_type;
+
+    inline vector() : super_t() {}
+
+    template<typename OtherVector>
+    inline vector(const OtherVector &other) : super_t(other) {}
+
+    inline vector(value_type v) : super_t(v) {}
+
+    inline vector(value_type xx, value_type yy)
+      : super_t(),
+        x(xx),
+        y(yy)
+    {}
+
+    inline vector cross(const vector &rhs) const
+    {
+      return detail::cross(*this, rhs);
+    } // end cross()
+
+    T x, y;
+}; // end vector<T,2>
+
+
 template<typename T>
 class vector<T,3> : public detail::vector_base<vector<T,3>, T, 3>
 {
@@ -42,6 +74,7 @@ class vector<T,3> : public detail::vector_base<vector<T,3>, T, 3>
 }; // end vector<T,3>
 
 
+typedef vector<float,2> float2;
 typedef vector<float,3> float3;
 
 
