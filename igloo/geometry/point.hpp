b3f215c3a753feb120a31459ab8bd12a749e4757
Jared Hoberock <jaredhoberock@gmail.com>
2014-01-14 01:22:13 -0800

    Add point::as_translation().

--- igloo/geometry/point.hpp
+++ igloo/geometry/point.hpp
@@ -39,6 +39,22 @@ class point : float3
       return point(*this - rhs);
     } // end operator-()
 
+    /*! \return the vector representing this point as a translation from the origin.
+     */
+    inline float3 as_translation() const
+    {
+      return (*this) - point(0,0,0);
+    } // end as_translation()
+
+    /*! Translation.
+     *  \param rhs The vector to add.
+     *  \return The point resulting from the given translation.
+     */
+    inline point operator+(const float3 &rhs) const
+    {
+      return point(*this + rhs);
+    } // end operator+()
+
     using super_t::operator[];
     using super_t::operator float *;
 
@@ -46,6 +62,17 @@ class point : float3
     using super_t::y;
     using super_t::z;
 }; // end point
+
+
+/*! Scalar multiply for points. 
+ *  \param s Scale factor.
+ *  \param x The point to scale.
+ *  \return x scaled by s.
+ */
+inline point operator*(float s, const point &x)
+{
+  return point(s * x.x, s * x.y, s * x.z);
+} // end operator*()
 
 
 } // end igloo
