c5f7f580a97a18c2efbe04fdbc29871fa8a96c4a
Jared Hoberock <jaredhoberock@gmail.com>
2014-01-28 23:17:55 -0800

    Add arithmetic to parametric

--- igloo/geometry/parametric.hpp
+++ igloo/geometry/parametric.hpp
@@ -18,11 +18,39 @@ class parametric : float2
      */
     inline parametric() : super_t() {}
 
-    /*! Creates a new point.
+    /*! Creates a new normal from a float2.
+     *  \param v The float2 to copy.
+     */
+    inline parametric(const float2 &v) : super_t(v) {}
+
+    /*! Creates a new parametric.
+     *  \param val The x & y coordinate of the parametric coordinates.
+     */
+    inline parametric(float val) : super_t(val) {}
+
+    /*! Creates a new parametric.
      *  \param x The x coordinate of the parametric coordinates.
      *  \param y The y coordinate of the parametric coordinates.
      */
     inline parametric(float x, float y) : super_t(x,y) {}
+
+    /*! parametric difference.
+     *  \param rhs The parametric to subtract.
+     *  \return The parametric resulting from the given subtraction.
+     */
+    inline parametric operator-(const parametric &rhs) const
+    {
+      return as_float2() - rhs;
+    } // end operator-()
+
+    /*! parametric addition.
+     *  \param rhs The parametric to add.
+     *  \return The parametric resulting from the given addition.
+     */
+    inline parametric operator+(const parametric &rhs) const
+    {
+      return as_float2() + rhs;
+    } // end operator+()
 
     using super_t::operator[];
     using super_t::operator float *;
@@ -30,7 +58,24 @@ class parametric : float2
 
     using super_t::x;
     using super_t::y;
+
+  private:
+    inline const float2 &as_float2() const
+    {
+      return *this;
+    } // end as_float2()
 }; // end parametric
+
+
+/*! parametric scalar multiply.
+ *  \param x The scalar to multiply by.
+ *  \param p The parametric to scale.
+ *  \return p multiplied by x.
+ */
+inline parametric operator*(float x, const parametric &p)
+{
+  return parametric(x * p.x, x * p.y);
+} // end operator*()
 
 
 } // end igloo
