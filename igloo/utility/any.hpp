32bb083ff179d98e443539bc1d83042bb4108f82
Jared Hoberock <jaredhoberock@gmail.com>
2016-10-26 00:09:31 -0500

    Introduce implementation of any

---
+++ igloo/utility/any.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <igloo/utility/any>
+
+namespace igloo
+{
+
+using bad_any_cast = std::experimental::fundamentals_v1::bad_any_cast;
+
+using any = std::experimental::fundamentals_v1::any;
+
+//using any_cast = std::experimental::fundamentals_v1::any_cast;
+
+
+} // end igloo
+
